1925bf0dc0bcf937952a685fd06c210d7421e5d8
Coder-Boy <adnanchembankolly@gmail.com>
2025-02-06 21:10:54 +0530

    changed

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,12 @@
-
 $(document).ready(function () {
     $('.footer-icons').mouseover(function () {
-        $(this).addClass('fa-bounce');  // Add the fa-flip class
-    });
-    
-    $('.footer-icons').mouseout(function () {
-        $(this).removeClass('fa-bounce');  // Remove the fa-flip class
+            $(this).addClass('fa-flip');
+            
+        }
+    );
+    $('.footer-icons').mouseout(function () { 
+        $(this).removeClass('fa-flip');
     });
 });
+    
+    
